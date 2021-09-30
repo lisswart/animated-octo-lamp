8e2667aac1c695e8f89dcc87505231d9e65cf395
Lisa Swart <arnoudlisa@gmail.com>
2021-09-30 00:22:52 -0400

    test associations with seed data

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -1,4 +1,6 @@
 class Preference < ApplicationRecord
   belongs_to :user
   has_many :timers
+  has_many :tasks, through: :timers
+  has_many :categories, through: :timers
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,81 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# puts "seeding users..."
+
+# 3.times do User.create(
+#   firstname: Faker::Name.first_name,
+#   lastname: Faker::Name.last_name,
+#   username: Faker::Twitter.unique.screen_name,
+#   email: Faker::Internet.unique.email,
+#   password_digest: Faker::Internet.unique.password
+#   )
+# end
+
+# puts "done seeding users..."
+
+# puts "seeding preferences..."
+
+# Preference.create(
+#   session_length: 25,
+#   break_length: 5,
+#   enable_long_break: true,
+#   long_break_length: 15,
+#   no_of_sessions_before_long_break: 3,
+#   user_id: 3
+# )
+
+# puts "done seeding preferences..."
+
+# puts "seeding tasks..."
+
+# Task.create(
+#   task_name: "first reading"
+# )
+
+# Task.create(
+#   task_name: "second reading"
+# )
+
+# Task.create(
+#   task_name: "take notes"
+# )
+
+# Task.create(
+#   task_name: "practice recall"
+# )
+
+# puts "done seeding tasks..."
+
+# puts "seeding categories..."
+
+# Category.create(
+#   category_label: "JavaScript"
+# )
+
+# Category.create(
+#   category_label: "Ruby"
+# )
+
+# Category.create(
+#   category_label: "React"
+# )
+
+# Category.create(
+#   category_label: "Rails"
+# )
+
+# Category.create(
+#   category_label: "Golang"
+# )
+
+# puts "done seeding categories..."
+
+# puts "seeding timers..."
+
+# 6.times do Timer.create(
+#   duration: 25,
+#   category_id: 1,
+#   task_id: 1,
+#   preference_id: 1
+#   )
+# end
+
+# puts "done seeding timers..."
